5e0dccdc0b590e04107699d510be86a7469806d0
Edgar Santana <santanaprogrammer@gmail.com>
2024-10-30 02:09:51 -0400

    Create DisplayFiveMessages.java
    ch1 1.2

---
+++ Chapter_1/DisplayFiveMessages.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.displayfivemessages;
+
+/**
+ * 1.2 (Display Five Messages) Write a program that displays I love Java five times.
+ */
+
+/**
+ *
+ * @author <Edgar Santana>
+ */
+public class DisplayFiveMessages {
+
+    public static void main(String[] args) {
+        System.out.println("I love Java");
+	System.out.println("I love Java");
+	System.out.println("I love Java");
+	System.out.println("I love Java");
+	System.out.println("I love Java");
+    }
+}
