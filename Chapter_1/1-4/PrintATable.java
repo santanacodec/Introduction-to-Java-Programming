5bbb83aed1929dbf29dbdbf5b541af7085a7c45f
Edgar Santana <santanaprogrammer@gmail.com>
2024-10-30 02:29:39 -0400

    Create PrintATable.java
    ch1 1.4

---
+++ Chapter_1/1-4/PrintATable.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.printatable;
+
+/**
+ * 1.4 (Print a table) Write a program that displays the following table:
+ * 
+ * a     a^2   a^3   a^4
+ * 1     1     1     1
+ * 2     4     8     16
+ * 3     9     27    81
+ * 4     14    64    256
+ */
+
+/**
+ *
+ * @author <Edgar Santana>
+ */
+public class PrintATable {
+
+    public static void main(String[] args) {
+        System.out.println("a     a^2   a^3   a^4");
+	System.out.println("1     1     1     1");
+	System.out.println("2     4     8     16");
+	System.out.println("3     9     27    81");
+	System.out.println("4     14    64    256");
+    }
+}
