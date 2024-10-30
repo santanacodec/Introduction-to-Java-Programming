acb676526a760390ce475f225df41c0c0946f0ad
Edgar Santana <santanaprogrammer@gmail.com>
2024-10-30 02:18:57 -0400

    organizing
    1-3

---
+++ Chapter_1/1-3/DisplayAPattern.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.displayapattern;
+
+/**
+ * 1.3 (Display a pattern) Write a program that displays the following pattern:
+ *      J
+ * J   aaa    v     vaaa
+ * J   J  aa    v v    a a
+ * J    aaaa     v     aaaa
+ */
+
+/**
+ *
+ * @author <Edgar Santana>
+ */
+public class DisplayAPattern {
+
+    public static void main(String[] args) {
+        System.out.println("     J");
+	System.out.println("J   aaa    v     vaaa");
+	System.out.println("J   J  aa    v v    a a");
+	System.out.println("J    aaaa     v     aaaa");
+    }
+}
