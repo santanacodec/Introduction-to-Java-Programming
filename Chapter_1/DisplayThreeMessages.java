c4e061e7b0fa7b3e44068cc731eb3b79dc38a14d
Edgar Santana <santanaprogrammer@gmail.com>
2024-10-30 02:04:51 -0400

    Create DisplayThreeMessages.java
    Ch1 1.1

---
+++ Chapter_1/DisplayThreeMessages.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.displaythreemessages;
+
+/** 1.1 (Display three messages) Write a program that displays Welcome to Java,
+ * Learning Java Now, and Programming is fun.
+ */
+
+/**
+ *
+ * @author <Edgar Santana>
+ */
+public class DisplayThreeMessages {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+	System.out.println("Learning Java Now");
+	System.out.println("Programming is fun");
+    }
+}
