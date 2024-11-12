24cf54bc8ba6eb3390e52e577d9a119b4af0108c
Edgar Santana <santanaprogrammer@gmail.com>
2024-11-12 05:34:18 -0500

    Create SummationOfASeries.java
    1-6

---
+++ Chapter_1/1-6/SummationOfASeries.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.summationofaseries;
+
+/**
+ * 1.6 (Summation of a series) Write a program that displays the result of
+ *	1+2+3+4+5+6+7+8+9+10
+ */
+
+/**
+ *
+ * @author <Edgar Santana>
+ */
+public class SummationOfASeries {
+
+    public static void main(String[] args) {
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10);
+    }
+}
