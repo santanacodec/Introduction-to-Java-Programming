ba0b42cc66ba0dab65d637516ef21791e099bf60
Edgar Santana <santanaprogrammer@gmail.com>
2024-11-20 07:38:28 -0500

    Create ComputeExpression.java
    listing 1-3

---
+++ Listings/1-3/ComputeExpression.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.computeexpression;
+
+/**
+ *
+ * @author <Edgar Santana>
+ */
+public class ComputeExpression {
+
+    public static void main(String[] args) {
+        System.out.print("(10.5 + 2 * 3) / (45 - 3.5) = ");
+	System.out.println((10.5 + 2 * 3) / (45 - 3.5));
+    }
+}
