f72d0decf8798f785ffccf342438a1ab549af4c4
Edgar Santana <santanaprogrammer@gmail.com>
2024-11-20 07:37:15 -0500

    Create WelcomeWithThreeMessages.java
    listing 1-2

---
+++ Listings/1-2/WelcomeWithThreeMessages.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.welcomewiththreemessages;
+
+/**
+ *
+ * @author <Edgar Santana>
+ */
+public class WelcomeWithThreeMessages {
+
+    public static void main(String[] args) {
+        System.out.println("Programming is fun!");
+	System.out.println("Fundamentals First");
+	System.out.println("Problem Driven");
+    }
+}
