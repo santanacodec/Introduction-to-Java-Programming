757301ff45b3a2477326de2cf7721d291d003139
Edgar Santana <santanaprogrammer@gmail.com>
2024-11-20 07:42:03 -0500

    Create ShowLogicErrors.java
    listing 1-6

---
+++ Listings/1-6/ShowLogicErrors.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.showlogicerrors;
+
+/**
+ *
+ * @author <Edgar Santana>
+ */
+public class ShowLogicErrors {
+
+    public static void main(String[] args) {
+        System.out.print("Celsius 35 is Fahrenheit degree ");
+	System.out.println((9/5) * 35 + 32);
+    }
+}
